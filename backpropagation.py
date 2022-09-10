7670bd82e655cf6fe1f4662c9fb8f20953c465aa
sbk128 <sdkhachane2000@gmail.com>
2022-09-10 11:57:36 +0530

    Gradients part added

--- backpropagation.py
+++ backpropagation.py
@@ -3,12 +3,14 @@ w = [-3.0, -1.0, 2.0]
 b = 1.0
 
 # Multiplying inputs by weights
-xw0 = x[0] * w[0]
-xw1 = x[1] * w[1]
-xw2 = x[2] * w[2]
+def weighted_sum(x, w, b):
+    xw0 = x[0] * w[0]
+    xw1 = x[1] * w[1]
+    xw2 = x[2] * w[2]
+    return xw0 + xw1 + xw2 + b
 
 # Adding weighted inputs and bias
-z = xw0 + xw1 + xw2 + b
+z = weighted_sum(x, w, b)
 
 # ReLU activation 
 y = max(z, 0)
@@ -47,5 +49,30 @@ drelu_dw1 = drelu_dwx1 * dmul_dw1
 drelu_dx2 = drelu_dwx2 * dmul_dx2
 drelu_dw2 = drelu_dwx2 * dmul_dw2
 
+# The partial derivatives combined into a vectore make up for the gradients.
+# The gradients can be represented as:
+dx = [drelu_dx0, drelu_dx1, drelu_dw2] # Gradients on inputs
+dw = [drelu_dw0, drelu_dw1, drelu_dw2] # Gradients on weights
+db = drelu_db   # Gradient on bias (which is 1)
 
-print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
+print(w, b)
+
+# For a single neuron layer there is no need of dx
+# We only need the gradients on the weights
+
+# Applying fraction of gradients to the original values
+w[0] += -0.001 * dw[0]
+w[1] += -0.001 * dw[1]
+w[2] += -0.001 * dw[2]
+
+print(w, b)
+
+z = weighted_sum(x, w, b)
+# ReLU activation
+y = max(z, 0)
+print(y)
+
+
+
+
+
