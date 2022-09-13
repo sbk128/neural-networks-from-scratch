f21e6c8f7a3d3b58218414bd35961c8bf9732ad5
sbk128 <sdkhachane2000@gmail.com>
2022-09-13 10:17:54 +0530

    different ways to achieve same results

--- backpropagation_layers.py
+++ backpropagation_layers.py
@@ -6,6 +6,11 @@ import numpy as np
 # The passed in gradients from next layer is considered as 1 only for this example
 dvalues = np.array([[1., 1., 1.]])
 
+# dvalues for a batch of samples
+dvalues = np.array([[1., 1., 1.],
+                    [2., 2., 2.],
+                    [3., 3., 3.]])
+
 # As usual there will be 3 neurons in each layer but there are 4 inputs per neuron
 # 3 neurons means 3 sets weights 
 # 4 weights for 4 inputs
@@ -18,7 +23,22 @@ dx1 = sum([dvalues[0][0]*weights[1][0], dvalues[0][1]*weights[1][1], dvalues[0][
 dx2 = sum([dvalues[0][0]*weights[2][0], dvalues[0][1]*weights[2][1], dvalues[0][2]*weights[2][2]])
 dx3 = sum([dvalues[0][0]*weights[3][0], dvalues[0][1]*weights[3][1], dvalues[0][2]*weights[3][2]])
 
-dinputs = [dx0, dx1, dx2, dx3]
+# Above code piece using numpy
+dx0 = sum(weights[0]*dvalues[0])
+dx1 = sum(weights[1]*dvalues[0])
+dx2 = sum(weights[2]*dvalues[0])
+dx3 = sum(weights[3]*dvalues[0])
+
+
+# dinputs = np.array([dx0, dx1, dx2, dx3])
+
+# This is just the dot product hence above piece of code can also be written as
+dinputs = np.dot(dvalues[0], weights.T)
+
+# This is just the dot product hence above piece of code for batch of gradients
+dinputs = np.dot(dvalues, weights.T)
+
 print(dinputs)
+
 
 
