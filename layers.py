019d5b4a39e00fdfed0c897214095d82a98b0e16
Sneha Khachane <sdkhachane2000@gmail.com>
2022-09-23 15:59:49 +0530

    Combind Class added

--- layers.py
+++ layers.py
@@ -42,6 +42,20 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+    def backward(self, dvalues):
+        # Create unintialized array
+        self.dinputs = np.empty_like(dvalues)
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+            enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calculating sample wise gradient and adding it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
 class Loss:
     def calculate(self, output, y):
         sample_losses = self.forward(output, y)
@@ -75,6 +89,30 @@ class Loss_CategoricalCrossentropy(Loss):
         self.dinputs = -y_true / dvalues
         # Normalizing gradients
         self.dinputs = self.dinputs / samples
+
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+    # Creates activation and loss function objects
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossentropy()
+
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)  # Output layer's activation function
+        self.output = self.activation.output 
+        return self.loss.calculate(self.output, y_true)
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        if len(y_true.shape) == 2:     # If labels are one hot encoded turn them into discrete values
+            y_true = np.argmax(y_true, axis=1) 
+        self.dinputs = dvalues.copy()
+        # Calculate gradients
+        self.dinputs[range(samples), y_true] -= 1
+        # Normalizing
+        self.dinputs = self.dinputs / samples
+
+
+
         
 
 
